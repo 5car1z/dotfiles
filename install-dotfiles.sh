0a894e2dfba2d8267cae8201f05f4c65a402208f
Scarlz <count.elmsley@protonmail.ch>
2015-07-01 20:17:59 +0100

    Updated install-dotfiles script to create ~/.vim folder and setup symlinks to a .vim folders contents in dotfiles.

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -6,9 +6,14 @@
 
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc bash_profile inputrc functions alias env vimrc"  		  # list of files/folders to symlink to homedir
+dir=~/dotfiles                   	                                  # dotfiles directory
+olddir=~/dotfiles_old
+
+vimdir=~/.vim
+oldvimdir=~/dotfiles_old/.vim
+                                 					  # old dotfiles backup directory
+dotfiles="bashrc bash_profile inputrc functions alias env vimrc"          # list of files/folders to symlink to homedir
+vimfiles="molokai_dark.vim"
 
 ##########
 
@@ -17,15 +22,38 @@ echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
 mkdir -p $olddir
 echo "done"
 
+# create .vim folder in dotfiles_old
+echo -n "Creating $oldvimdir for backup of any existing vim config files in ~/.vim ..."
+mkdir -p $oldvimdir
+echo "done"
+
+# create vim configuration folder in homedir 
+echo -n "Creating vim configuration folder in ~ ..."
+mkdir -p $vimdir
+echo "done"
+
 # change to the dotfiles directory
 echo -n "Changing to the $dir directory ..."
 cd $dir
 echo "done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
-for file in $files; do
+for dotfile in $dotfiles; do
     echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+    mv ~/.$dotfile ~/dotfiles_old/
+    echo "Creating symlink to $dotfile in home directory."
+    ln -s $dir/$dotfile ~/.$dotfile
+done
+
+# change to the vimfiles directory
+echo -n "Changing to the $vimdir directory ..."
+cd $vimdir
+echo "done"
+
+# move any existing vimfiles in ~/.vim folder to ~/dotfiles_old/.vim directory, then create symlinks from ~/.vim to any files in the ~/dotfiles/.vim directory, specified in $vimfiles
+for vimfile in $vimfiles; do
+    echo "Moving any existing vim config files from ~/.vim to $oldvimdir"
+    mv ~/.vim/$vimfiles $oldvimdir
+    echo "Creating symlink to $vimfile in home directory"
+    ln -s $dir/.vim/$vimfile $vimdir
 done
