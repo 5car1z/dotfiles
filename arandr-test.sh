ca1b990ed6933a3d2d3b25f0cd04da6128fdb7c9
5car1z <count.elmsley@protonmail.ch>
2016-01-02 13:11:27 +0000

    Added an automatically generated arandr script for tracking - not currently used.

---
+++ arandr-test.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-1 --primary --mode 3440x1440 --pos 0x0 --rotate normal --output DVI-I-1 --off --output DVI-D-1 --mode 1920x1080 --pos 3440x168 --rotate normal --output HDMI-1 --off
