244a8f7327d7af7688cc85d5a14bd9dc8f9acfe8
5car1z <count.elmsley@protonmail.ch>
2016-03-16 12:59:46 +0000

    install-arch-dotfiles.dependencies.sh - made pip and cheat install correctly, added tmux plugin manager submodule and parent plugins/ directory.

--- install-arch-dotfile-dependencies.sh
+++ install-arch-dotfile-dependencies.sh
@@ -4,13 +4,13 @@
 
 echo -n "Installing Pacman packages..."
 sudo pacman -Syy && sudo pacman -Syu
-sudo pacman -S stow curl cowsay fortune-mod jq vim mpv jack2 compton
+sudo pacman -S stow curl cowsay fortune-mod jq vim mpv jack2 compton tmux
 echo -n "done"
 
 echo -n "Installing pip and cheat..."
 wget -v https://bootstrap.pypa.io/get-pip.py -P ~
-python ~/get-pip.py 
-pip install cheat 
+sudo python ~/get-pip.py 
+sudo pip install cheat 
 echo -n "done"
 
 
