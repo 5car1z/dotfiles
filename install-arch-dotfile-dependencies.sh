470724791afa92c8d21e471348b9a512dfae3d48
5car1z <count.elmsley@protonmail.ch>
2016-03-16 11:50:16 +0000

    install-arch-dotfile-dependencies.sh - added stow to pacman install.

--- install-arch-dotfile-dependencies.sh
+++ install-arch-dotfile-dependencies.sh
@@ -4,13 +4,13 @@
 
 echo -n "Installing Pacman packages..."
 sudo pacman -Syy && sudo pacman -Syu
-sudo pacman -S curl cowsay fortune-mod jq vim mpv jack2 compton
+sudo pacman -S stow curl cowsay fortune-mod jq vim mpv jack2 compton
 echo -n "done"
 
 echo -n "Installing pip and cheat..."
 wget -v https://bootstrap.pypa.io/get-pip.py -P ~
 python ~/get-pip.py 
-sudo pip install cheat 
+pip install cheat 
 echo -n "done"
 
 
