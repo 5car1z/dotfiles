6276e182a209a0a62cbc1c08493e9e74784ffc9b
5car1z <count.elmsley@protonmail.ch>
2016-06-20 13:59:52 +0100

    install-debian-ubuntu-dotfile-dependencies.sh - added htop i3 and virtualenv packages, plus some other changes.

--- install-debian-ubuntu-dotfile-dependencies.sh
+++ install-debian-ubuntu-dotfile-dependencies.sh
@@ -2,15 +2,15 @@
 
 # install dotfile dependencies for Debian / Ubuntu based systems. 
 
-echo -n "Installing Apt packages..."
+echo -n "Installing Apt packages...\n"
 sudo apt-get -y update && sudo apt-get -y upgrade 
-sudo apt-get install stow curl cowsay fortune-mod jq vim tmux ranger
-echo -n "done"
+sudo apt-get install stow curl cowsay fortune-mod jq vim tmux ranger htop i3 
+echo -n "Done"
 
-echo -n "Installing pip and cheat..."
+echo -n "Installing pip and cheat... \n"
 wget -v https://bootstrap.pypa.io/get-pip.py -P ~
 sudo python ~/get-pip.py 
-sudo pip install cheat 
-echo -n "done"
+sudo pip install cheat virtualenv
+echo -n "Done"
 
 
