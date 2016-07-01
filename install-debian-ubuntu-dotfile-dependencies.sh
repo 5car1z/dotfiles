f9949556d8feaa20ffdb778d86f729375a1dc8ce
5car1z <count.elmsley@protonmail.com>
2016-07-01 20:32:35 +0100

    install-debian-ubuntu-dotfile-dependencies.sh - updated slightly.

--- install-debian-ubuntu-dotfile-dependencies.sh
+++ install-debian-ubuntu-dotfile-dependencies.sh
@@ -4,14 +4,14 @@
 
 echo -n "Installing Apt packages...\n"
 sudo apt-get -y update && sudo apt-get -y upgrade 
-sudo apt-get install stow curl cowsay fortune-mod jq vim tmux ranger htop i3 
-echo -n "Done \n"
+sudo apt-get install stow curl cowsay fortune-mod jq vim mpv tmux ranger htop i3 
+echo -n "Done.\n"
 
 echo -n "Installing pip and cheat... \n"
 wget -v https://bootstrap.pypa.io/get-pip.py -P ~
 sudo python ~/get-pip.py 
 sudo pip install cheat virtualenv
 rm ~/get-pip.py
-echo -n "Done \n"
+echo -n "Done.\n"
 
 
