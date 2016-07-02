4cfac902bec26d90722edc204e0a84a054b1cc58
5car1z <count.elmsley@protonmail.com>
2016-07-02 15:48:26 +0100

    install-arch-dotfile-dependencies.sh - fixed some formatting.

--- install-arch-dotfile-dependencies.sh
+++ install-arch-dotfile-dependencies.sh
@@ -2,10 +2,10 @@
 
 # install dotfile dependencies for Arch Linux based systems. 
 
-echo -n "Installing Pacman packages..."
+echo -n "Installing Pacman packages...\n"
 sudo pacman -Syy && sudo pacman -Syu
 sudo pacman -S stow curl cowsay fortune-mod jq vim mpv jack2 compton tmux ranger htop 
-echo -n "Done."
+echo -n "Done.\n"
 
 echo -n "Installing pip and cheat... \n"
 wget -v https://bootstrap.pypa.io/get-pip.py -P ~
